d9841cd08f5d700a0f6c74130a09efc1f8572840
KAJUGA Daniels <kajugadaniels@gmail.com>
2025-02-27 15:11:55 +0200

    New changes

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,7 +1,7 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
-    darkMode: ["class"],
+    // darkMode: ["class"],
     content: [
         "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
         "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
@@ -9,6 +9,9 @@ const config: Config = {
     ],
     theme: {
         extend: {
+            fontSize: {
+                sm: '0.875rem', 
+            },
             colors: {
                 background: 'hsl(var(--background))',
                 foreground: 'hsl(var(--foreground))',
