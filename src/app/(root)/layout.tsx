25c327df06f8e6c10f5fdc19da4942184884e1f9
KAJUGA Daniels <kajugadaniels@gmail.com>
2025-02-27 17:49:38 +0300

    Setup home layout

---
+++ src/app/(root)/layout.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Layout = ({ children }: { children: React.ReactNode }) => {
+    return (
+        <div className="h-full w-full">
+            Navbar
+            <main
+                className={`h-full flex w-full flex-col`}
+                style={{ paddingTop: '52px' }}
+            >
+                {children}
+            </main>
+        </div>
+    )
+}
+
+export default Layout
