3738154e9fbb689e3ef8a8503bb4dde2e238c794
KAJUGA Daniels <kajugadaniels@gmail.com>
2025-02-28 12:16:13 +0200

    New changes

--- next.config.ts
+++ next.config.ts
@@ -4,12 +4,6 @@ const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
       {
-        protocol: "http",
-        hostname: "127.0.0.1",
-        port: "8000",
-        pathname: "/**",
-      },
-      {
         protocol: "https",
         hostname: "example.com",
         port: "",
